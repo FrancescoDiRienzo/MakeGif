ebf4dcf393fe3c960f33be5bb5c897d596d603dd
danieleago <agostidaniele@gmail.com>
2016-10-15 19:27:49 +0200

    Add file with definition of constants

---
+++ constant.h
@@ -0,0 +1,16 @@
+
+// constant for error_print
+#define ERROR_BACKGROUND_FILE		0
+#define ERROR_INPUT_FILE			1
+#define ERROR_OUTPUT_FILE			2	
+#define ERROR_PARAMITERS			3
+#define ERROR_MULTIPLE_PARAMITERS	4
+//
+
+//constant for type of animation
+#define CIRCLE		1
+#define LINEAR		2
+#define SIN			3
+#define COS 		4
+#define DIAGONAL	5
+//	
